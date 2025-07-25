d86af9ec9c09d099c67716e4ed8e356b6efa6117
goldshoot0720 <goldshoot0720@gmail.com>
2025-07-25 23:34:30 +0800

    20250725 v3

--- abuhg17-git.bat
+++ abuhg17-git.bat
@@ -4,6 +4,5 @@ git add .
 git commit -m "20250725 v3"
 git remote add origin https://github.com/abuhg17/flasksqlite.git
 git checkout -b main
-git push -u origin main
- --force
+git push -u origin main --force
 pause
